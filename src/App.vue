49c687b5952e9ba9c9a60848e53750c2955b0776
P Aswini Kumar <aswini123p@gmail.com>
2021-03-28 05:59:37 +0530

    Haxplore Project

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+<div id="app">
+  <Slider/>
+  </div>
+</template>
+
+<script>
+import Slider from './components/Slider.vue'
+// import About from './components/About.vue'
+export default {
+  name: "app",
+  components: {
+    Slider,
+    // About,
+  }
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  /* margin-top: 60px; */
+}
+body {
+  margin: 0px;
+}
+</style>
